b178c29fa76d360160c4bb04025c2209776aa49f
abdulrhmanmagdy1 <abdulrhmanmagdy13@gmail.com>
2025-09-17 12:12:50 -0700

    last modified

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, APP_INITIALIZER } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -17,7 +17,14 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled'
+      })
+    ),
+    provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([cacheInterceptor, authInterceptor, errorInterceptor, loadingInterceptor])),
     {
       provide: APP_INITIALIZER,
